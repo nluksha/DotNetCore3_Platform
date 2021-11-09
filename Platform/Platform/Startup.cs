0b48ad7b076d13935c06d83843eecb2f58d33066
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-09 20:55:41 +0300

    Add options for middleware

--- Platform/Platform/Startup.cs
+++ Platform/Platform/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,15 +17,31 @@ namespace Platform
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MessageOptions>(options =>
+           {
+               options.CityName = "Albany";
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<MessageOptions> msgOptions)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) =>
+            {
+                if(context.Request.Path == "/location")
+                {
+                    MessageOptions options = msgOptions.Value;
+                    await context.Response.WriteAsync($"{options.CityName}, {options.CountryName}");
+                } else
+                {
+                    await next();
+                }
+            });
 
             app.Map("/branch", branch =>
             {
