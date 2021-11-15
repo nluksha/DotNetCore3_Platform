4834005bf559ee6660a99dfed5f70fbc2c4972be
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-15 09:45:09 +0300

    Add Transient service

---
+++ Platform/Platform/Servises/GuidService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Platform.Servises
+{
+    public class GuidService: IResponseFormatter
+    {
+        private Guid guid = Guid.NewGuid();
+
+        public async Task Format(HttpContext context, string content)
+        {
+            await context.Response.WriteAsync($"Guid: {guid}\n{content}");
+        }
+    }
+}
