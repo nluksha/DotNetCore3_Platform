5ac570b28e7b6633a97cf83210566a784813f241
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-16 07:58:08 +0300

    Resert project

--- Platform/Platform/Startup.cs
+++ Platform/Platform/Startup.cs
@@ -27,73 +27,22 @@ namespace Platform
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            /*
-            services.AddScoped<IResponseFormatter>(serviceProvider =>
-            {
-                string typeName = Configuration["services:IResponseFormatter"];
-
-                return (IResponseFormatter)ActivatorUtilities
-                .CreateInstance(serviceProvider, typeName == null ? typeof(GuidService) : Type.GetType(typeName, true));
-            });
-            */
-
-            services.AddScoped<IResponseFormatter, TimeResponseFormatter>();
-            services.AddScoped<IResponseFormatter, HtmlResponseFromatter>();
-            services.AddScoped<IResponseFormatter, GuidService>();
-            services.AddScoped<ITimeStamper, DefaultTimeStamper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseRouting();
 
-            app.UseMiddleware<WeatherMiddleware>();
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/middleware/function")
-                {
-                    IResponseFormatter formatter = context.RequestServices.GetService<IResponseFormatter>();
-                    await formatter.Format(context, "Middleware Function: It is anowing in Chicago");
-                }
-                else
-                {
-                    await next();
-                }
-            });
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/single", async context =>
-                {
-                    IResponseFormatter formatter = context.RequestServices
-                    .GetService<IResponseFormatter>();
-
-                    await formatter.Format(context, "Single service");
-                });
-
                 endpoints.MapGet("/", async context =>
                 {
-                    IResponseFormatter formatter = context.RequestServices
-                    .GetServices<IResponseFormatter>()
-                    .First(f => f.RichOutput);
-
-                    await formatter.Format(context, "Multiple services");
+                    await context.Response.WriteAsync("Hello  World!");
                 });
 
-                endpoints.MapEndpoint<WeatherEndpoint>("/endpoint/class");
-
-                endpoints.MapGet("/endpoint/function", async context =>
-                {
-                    IResponseFormatter formatter = context.RequestServices.GetService<IResponseFormatter>();
-                    await formatter.Format(context, $"Endpoint Function: It is sunny in LA");
-                });
             });
         }
     }
